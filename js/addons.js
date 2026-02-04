7d0b57762bc5b167c8de2368b4d57df0d40155b7
David Zsargo <dzsargo@scissero.com>
2026-02-04 21:25:53 +0100

    Cleanups

--- js/addons.js
+++ js/addons.js
@@ -11,10 +11,6 @@ document.addEventListener('alpine:init', () => {
     };
     let cleanupHtml = (el, html) => ([...el.querySelectorAll(':scope > *')].forEach(e => e.remove()), html);
     let template = (url, options) => _import('js/modules/template.mjs').then(m => m(url, options));
-    let setValue = function (obj, key, value, defaultValue) {
-        obj[key] = value !== undefined ? value : defaultValue;
-        return obj[key];
-    };
     // Directives
     Alpine.directive('destroy', (el, { expression }, { evaluateLater, cleanup }) => {
         const clean = evaluateLater(expression);
@@ -25,23 +21,11 @@ document.addEventListener('alpine:init', () => {
     Alpine.magic('cleanup', () => cleanupHtml);
     Alpine.magic('stringify', () => JSON.stringify);
     Alpine.magic('assign', () => Object.assign);
-    Alpine.magic('pathname', () => () => location.pathname.replace('/',''));
     Alpine.magic('html', () => (el, html, name) => el.querySelector(`[x-ignore${name ? '="' + name + '"' : ''}]`).innerHTML = html);
     Alpine.magic('log', () => function () {
         console.log(...arguments);
         return [...arguments].pop();
     });
-    Alpine.magic('set', () => setValue);
-    Alpine.magic('fetch', () => function (url, options) {
-        return fetch(url, options)
-            .then(res => ({ res }))
-            .then(obj => obj.res.clone().json().then(json => Object.assign(obj, { json })))
-            .then(obj => obj.res.clone().text().then(text => Object.assign(obj, { text })));
-    });
-
-    Alpine.magic('next', () => ({
-        set: (targetObj, key, value, defaultValue) => obj => (setValue(targetObj, key, value.split('.').reduce((o, i) => o[i], obj), defaultValue), obj),
-    }));
     Alpine.magic('template', () => (el, url, options) => template(url, options).then(html => el.innerHTML = cleanupHtml(el, html)));
     // Data
     Alpine.data('load', modulesObject => ({
@@ -51,7 +35,7 @@ document.addEventListener('alpine:init', () => {
             _import(Object.values(modulesObject)).then(modules => {
                 modules.forEach((mod, i) => {
                     this[Object.keys(modulesObject)[i]] = mod;
-                });  
+                });
 
                 this.load_done = true;
             });
